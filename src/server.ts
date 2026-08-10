09820ed3bbde5ffaec6e1cf811b36b5cb705cebd
shivmSingh1 <shivams7380@gmail.com>
2026-08-10 07:38:26 +0000

    add file based config

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,9 @@
 import app from "./app";
 import logger from "./config/logger";
+import config from "config"
 
 const startServer = () => {
-    const PORT = 5502;
+    const PORT: number = config.get("server.port");
     try {
         app.listen(PORT, () => logger.info(`Listening on port ${PORT}`));
     } catch (err: unknown) {
