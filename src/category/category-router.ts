5910d0e101c5282f2a3e58735433e60692736a1a
shivmSingh1 <shivams7380@gmail.com>
2026-08-16 13:56:58 +0000

    add authorization

--- src/category/category-router.ts
+++ src/category/category-router.ts
@@ -5,12 +5,16 @@ import { CategoryService } from "./category-service";
 import logger from "../config/logger";
 import { asyncWrapper } from "../common/utils/wrapper";
 import authenticate from "../common/middlewares/authenticate";
+import { canAccess } from "../common/middlewares/canAccess";
+import { Roles } from "../common/constants";
+
 
 const categoryRouter = express.Router();
 
 const categoryService = new CategoryService();
 const categoryController = new CategoryController(categoryService, logger);
 
-categoryRouter.post("/", authenticate, categoryValidator, asyncWrapper(categoryController.create));
+categoryRouter.post("/", authenticate, canAccess([Roles.ADMIN]), categoryValidator, asyncWrapper(categoryController.create));
 
 export default categoryRouter;
+
